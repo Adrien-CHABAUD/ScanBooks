81931a75f678432629a38eefeb2253976bb38b69
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2022-11-29 13:01:48 -0500

    Initial Commit

---
+++ ScanBooks/ScanBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBooksApp.swift
+//  ScanBooks
+//
+//  Created by Adrien CHABAUD on 2022-11-29.
+//
+
+import SwiftUI
+
+@main
+struct ScanBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
